f2a0e615b23cfbeedf014dda8aa3bf08114345d6
272727ana <96492887+272727ana@users.noreply.github.com>
2023-10-13 18:10:27 +0200

    Update ExamResult.java

--- L3/ExamResult.java
+++ L3/ExamResult.java
@@ -1,7 +1,9 @@
-class ExamResult {
+package lecture3PL;
+
+public class myNumber {
 	private int num;
 	private int num2;
-	public ExamResult() {
+	public myNumber() {
 		num=0;
 		num2=100;
 	}
@@ -12,11 +14,23 @@ class ExamResult {
 		num=i;
 		num2=10*i;
 	}
-	public static void main (String [] ar) {
-		ExamResult A=new ExamResult();
+	int getNum2() {
+		return num2;
+	}
+	void description() {
+		System.out.println("The value of num is" +this.getNum() );
+		System.out.println("The value of num2 is" + this.getNum2());
+	}
+	float getAverage() {
+		return (num+num2/2);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		myNumber A=new myNumber();
 		System.out.println(A.getNum());
 		A.setNum(10);
 		System.out.println(A.getNum());
 		System.out.println(A.num2);
 	}
+
 }
