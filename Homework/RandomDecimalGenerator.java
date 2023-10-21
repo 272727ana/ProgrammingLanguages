265d195deebfa7a5a4645c17c4fc4cbfee52c931
272727ana <96492887+272727ana@users.noreply.github.com>
2023-10-21 21:15:32 +0200

    Create RandomDecimalGenerator.java

---
+++ Homework/RandomDecimalGenerator.java
@@ -0,0 +1,23 @@
+import java.util.random;
+
+public class RandomDecimalGenerator {
+    public static void main(String[] args) {
+    
+    double randomNumber = generateRandomDecimal();
+    System.out.printf("RandomNumber: " %2f%n , randomNumber);
+
+    int integerPart = (int) randomNumber;
+    System.out.println("IntegerPart: " + integerPart);
+
+    double decimalPart = randomNumber - integerPart;
+    System.out.printf("DecimalPart: %2f%n", decimalPart);
+
+    char characterFromCode = (char) integerPart;
+    System.out.println("Character from CODE: " + characterFromCode);    
+        }
+
+private static double generateRandomDecimal() {
+    Random random = new Random();
+    return random.nextDouble() * 100;
+}
+}
