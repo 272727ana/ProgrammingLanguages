b9940d1521d2be5b3eb18c3e105564c37e21bda1
272727ana <96492887+272727ana@users.noreply.github.com>
2023-10-27 18:49:38 +0200

    Animal.java

---
+++ L5/Animal.java
@@ -0,0 +1,23 @@
+class Animal {
+    public void onomatopoeia() {
+        System.out.println("Animal sound");
+    }
+}
+
+class Dog extends Animal {
+    public void onomatopoeia() {
+        System.out.println("Woof");
+    }
+}
+
+class Lion extends Animal {
+    public void onomatopoeia() {
+        System.out.println("Roar");
+    }
+}
+
+class Frog extends Animal {
+    public void onomatopoeia() {
+        System.out.println("Ribbit");
+    }
+}
